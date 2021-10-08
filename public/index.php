bfac10153f50c56d1372d8e2ace3ac40c9592393
Anton Goleusov <antongoleusov@gmail.com>
2021-10-08 13:33:14 +0300

    Изменено имя проекта на говорящее Папка .idea добавлена в gitignonre Перепроверены неймспейсы Объект создан через конструктор У свойств класса задан тип

--- public/index.php
+++ public/index.php
@@ -4,11 +4,8 @@ require __DIR__ . '/../vendor/autoload.php';
 
 use MsBalance\User;
 
-function getUser($idUser, $accNumber, $Balance){
-    $User = new User();
-    $User->idUser = $idUser;
-    $User->accNumber = $accNumber;
-    $User->Balance = $Balance;
-    print_r($User);
+function getUser(int $idUser, int $accNumber, int $balance, string $name){
+    $user = new User($idUser, $accNumber, $balance, $name);
+    print_r($user);
 }
-getUser(1, 1, 15);
+getUser(1, 1, 15, 'Anton');
