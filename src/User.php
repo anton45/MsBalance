88fb1ce9573a4024f8816cff186c5548078c82df
Anton Goleusov <antongoleusov@gmail.com>
2021-10-08 17:51:56 +0300

    Добавлен класс WalletManager

--- src/User.php
+++ src/User.php
@@ -7,10 +7,8 @@ class User{
     private int $userId;
     private int $balance;
 
-
     public function __construct(int $id, int $userId, int $balance)
     {
-
         $this->id = $id;
         $this->userId = $userId;
         $this->balance = $balance;
--- src/WalletManager.php
+++ src/WalletManager.php
@@ -4,5 +4,12 @@ namespace MsBalance;
 
 class WalletManager
 {
+    private int $iserId;
+    private int $balance;
 
+
+    public function __construct(array $json)
+    {
+
+    }
 }
