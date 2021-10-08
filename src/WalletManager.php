4d3feb4ff569a88d25e4b703257e0804e996d8fa
Anton Goleusov <antongoleusov@gmail.com>
2021-10-08 18:00:37 +0300

    Добавлена структура методов в WalletManager

--- src/WalletManager.php
+++ src/WalletManager.php
@@ -6,10 +6,28 @@ class WalletManager
 {
     private int $iserId;
     private int $balance;
+    private int $money;
 
 
     public function __construct(array $json)
     {
 
     }
+    public function balance(int $userId, int $money): bool
+    {
+
+    }
+/*    public function add(int $userId, int $money): bool
+    {
+
+    }
+    public function delete(int $userId, int $money): bool
+    {
+
+    }
+    public function moneyTransfer(int $userId, int $userId, int $money): bool
+    {
+
+    }
+ */
 }
